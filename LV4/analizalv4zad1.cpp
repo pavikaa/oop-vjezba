1f3d905fb59afaca3fdacc5c16ad9e2681c1bf71
Marko Pavičić <mpavicic99@gmail.com>
2019-12-01 15:11:52 +0100

    LV4 Analiza

---
+++ LV4/analizalv4zad1.cpp
@@ -0,0 +1,50 @@
+﻿//Napiši predloške funkcija Max() i Min() koje kao parametre primaju
+//jednodimenzionalna polja i broj elemenata u polju.Funkcije pronalaze i kao
+//rezultat vraćaju najveći odnosno najmanji element polja.Funkciju testirati na
+//tri dinamički alocirana polja različitog tipa.
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+template<class Tip>
+Tip max(Tip* polje, int n)
+{
+	Tip maxElement = polje[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (polje[i] > maxElement)
+			maxElement = polje[i];
+	}
+	return maxElement;
+}
+template<class Tip>
+Tip min(Tip* polje, int n)
+{
+	Tip minElement = polje[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (polje[i] < minElement)
+			minElement = polje[i];
+	}
+	return minElement;
+}
+template <class Tip>
+void random(Tip* polje, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		polje[i] = (Tip)rand()/150;
+	}
+}
+int main()
+{
+	srand(time(NULL));
+	int* polje1 = new int[8];
+	random(polje1, 8);
+	double* polje2 = new double[12];
+	random(polje2, 12);
+	float* polje3 = new float[10];
+	random(polje3, 10);
+	std::cout << "Max elementi polja su : " << max(polje1, 8) << " " << max(polje2, 12) << " " << max(polje3, 10) << std::endl;
+	std::cout << "Min elementi polja su : " << min(polje1, 8) << " " << min(polje2, 12) << " " << min(polje3, 10) << std::endl;
+
+}
---
+++ LV4/analizalv4zad2.cpp
@@ -0,0 +1,59 @@
+﻿/*Napiši predložak klase UređeniPar() koja predstavlja uređeni par dvaju
+objekata.Podatkovni članovi su dva dinamički alocirana objekta, a klasa mora
+imati konstruktore(default, parametarski i kopije), destruktor, operator = i
+operatore usporedbe.U glavnoj funkciji testirati funkcionalnost klase za tri
+različita tipa podatka*/
+#include<iostream>
+
+template <class Tip>
+class UredeniPar
+{
+	Tip* objekt1;
+	Tip* objekt2;
+public:
+	UredeniPar() :objekt1(new Tip), objekt2(new Tip) {}
+	UredeniPar(Tip objekt1,Tip objekt2):objekt1(new Tip(objekt1)),objekt2(new Tip(objekt2)){}
+	UredeniPar(const UredeniPar& ref)
+	{
+		this->objekt1 = new Tip;
+		this->objekt2 = new Tip;
+		*objekt1 = *ref.objekt1;
+		*objekt2 = *ref.objekt2;
+	}
+	~UredeniPar()
+	{
+		delete objekt1;
+		delete objekt2;
+	}
+	UredeniPar& operator=(const UredeniPar& ref)
+	{
+		if (this != &ref)
+		{
+			this->objekt1 = new Tip;
+			this->objekt2 = new Tip;
+			*objekt1 = *ref.objekt1;
+			*objekt2 = *ref.objekt2;
+		}
+		return *this;
+	}
+	friend bool operator== (const UredeniPar& ref1, const UredeniPar& ref2)
+	{
+		return ((*ref1.objekt1 == *ref2.objekt1) && (*ref1.objekt2 == *ref2.objekt2));
+	}
+	friend bool operator!= (const UredeniPar& ref1, const UredeniPar& ref2)
+	{
+		return !(ref1==ref2);
+	}
+};
+
+int main()
+{
+	UredeniPar<int> prvi(2, 5);
+	UredeniPar<int> drugi(prvi);
+	std::cout << (prvi == drugi) << std::endl;
+	UredeniPar<int>treci(1, 4);
+	UredeniPar<int>cetvrti(2, 2);
+	UredeniPar<int>peti;
+	peti = cetvrti;
+	std::cout << (peti != treci) << std::endl;
+}
