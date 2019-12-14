2010b0d2724fb8c84059af022871917f74e78064
Marko Pavičić <mpavicic99@gmail.com>
2019-12-14 15:56:23 +0100

    LV6

---
+++ LV6/lv6zad1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lv6zad1
+{
+    public partial class Form1 : Form
+    {
+        double a, b, c;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void povrsina_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(tB_a.Text, out a) || a < 0)
+                MessageBox.Show("Pogrešan unos stranice a.");
+            else if (!double.TryParse(tB_b.Text, out b) || b < 0)
+                MessageBox.Show("Pogrešan unos stranice b.");
+            else if (!double.TryParse(tB_c.Text, out c) || c < 0)
+                MessageBox.Show("Pogrešan unos stranice c.");
+            else if (a >= b + c || b >= a + c || c >= a + b)
+            {
+                MessageBox.Show("Pogrešan unos trokuta.");
+            }
+            else
+            {
+                double s = (a + b + c)/2;
+                double P = System.Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+                rezultat.Text =P.ToString();
+            }
+        }
+
+        private void pravokutan_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(tB_a.Text, out a) || a < 0)
+                MessageBox.Show("Pogrešan unos stranice a.");
+            else if (!double.TryParse(tB_b.Text, out b) || b < 0)
+                MessageBox.Show("Pogrešan unos stranice b.");
+            else if (!double.TryParse(tB_c.Text, out c) || c < 0)
+                MessageBox.Show("Pogrešan unos stranice c.");
+            else if (a >= b + c || b >= a + c || c >= a + b)
+            {
+                MessageBox.Show("Pogrešan unos trokuta.");
+            }
+            if (a == System.Math.Sqrt(b * b + c * c) || b == System.Math.Sqrt(a * a + c * c) || c == System.Math.Sqrt(b * b + a * a))
+                MessageBox.Show("Trokut je pravokutan.");
+            else
+                MessageBox.Show("Trokut nije pravokutan.");
+        }
+
+        private void quit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void opseg_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(tB_a.Text, out a)|| a<0)
+                MessageBox.Show("Pogrešan unos stranice a.");
+            else if (!double.TryParse(tB_b.Text, out b)||b<0)
+                MessageBox.Show("Pogrešan unos stranice b.");
+            else if (!double.TryParse(tB_c.Text, out c)||c<0)
+                MessageBox.Show("Pogrešan unos stranice c.");
+            else if (a >= b + c || b >= a + c || c >= a + b)
+            {
+                MessageBox.Show("Pogrešan unos trokuta.");
+            }
+            else
+            {
+                rezultat.Text = (a + b + c).ToString();
+            }
+        }
+    }
+}
---
+++ LV6/lv6zad2.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace lv6zad2
+{
+    public partial class Form1 : Form
+    {
+        List<Kontakt> listKontakti = new List<Kontakt>();
+        string path = "D:\\kontakti.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool empty = false;
+            if (textBox1.Text == "" ||textBox2.Text == "" || textBox3.Text == "")
+            {
+                empty = true;
+                MessageBox.Show("Neko od polja ostalo je prazno.");
+            }
+            else
+                empty = false;
+            
+            if (!empty)
+            {
+                Kontakt K = new Kontakt(textBox1.Text, textBox2.Text, textBox3.Text);
+                listKontakti.Add(K);
+                lb_Kontakti.DataSource = null;
+                lb_Kontakti.DataSource = listKontakti;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(@path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] parts = line.Split('\t');
+                    Kontakt K = new Kontakt(parts[0], parts[1], parts[2]);
+                    listKontakti.Add(K);
+                }
+                lb_Kontakti.DataSource = null;
+                lb_Kontakti.DataSource = listKontakti;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void izlaz_Click(object sender, EventArgs e)
+        {
+            using (System.IO.StreamWriter write = new System.IO.StreamWriter(@path))
+            {
+                foreach (Kontakt k in listKontakti)
+                {
+                    {
+                        write.WriteLine(k.ToString());
+                    }
+                }
+            }
+            Application.Exit();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            foreach (Kontakt k in listKontakti)
+                if (k.ToString().Contains(textBox4.Text))
+                    MessageBox.Show("Pronađen.");
+                else MessageBox.Show("Nije pronađen.");
+            }
+
+        private void lb_Kontakti_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+    class Kontakt
+    {
+        #region data_members
+        private string ime;
+        private string prezime;
+        private string brojTelefona;
+        #endregion
+        #region public_methods
+        public Kontakt()
+        {
+            ime = "Ivan";
+            prezime = "Ivanić";
+            brojTelefona = "0000000000";
+        }
+        public Kontakt(string i, string p, string b)
+        {
+            ime = i;
+            prezime = p;
+            brojTelefona = b;
+        }
+        public override string ToString()
+        {
+            return ime + "\t" + prezime + "\t" + brojTelefona;
+        }
+        #endregion
+    }
+}
