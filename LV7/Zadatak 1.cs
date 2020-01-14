297717842c85b823a29c812c92da313cc7cee0a9
Marko Pavičić <mpavicic99@gmail.com>
2020-01-14 18:19:20 +0100

    LV7

---
+++ LV7/Zadatak 1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LV7_Zad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<BankAccount> bankList= new List<BankAccount>();
+            List<MobileAccount> mobileList= new List<MobileAccount>();
+            BankAccount bankAccount = new BankAccount(1500, 1000, "Visa");
+            BankAccount bankAccount2 = new BankAccount(1950, 3000, "American");
+            MobileAccount mobileAccount = new MobileAccount("+385950000000", 57.5, 0.50);
+            MobileAccount mobileAccount2 = new MobileAccount("+385950000001", 67.5, 0.40);
+            bankList.Add(bankAccount);
+            bankList.Add(bankAccount2);
+            mobileList.Add(mobileAccount);
+            mobileList.Add(mobileAccount2);
+            foreach(BankAccount account in bankList)
+            {
+                Console.WriteLine(account.getIznos() + "\t");
+                account.addToIznos(50);
+                Console.WriteLine(account.getIznos() + "\t");
+                account.subtractFromIznos(20);
+                Console.WriteLine(account.getIznos()+"\t");
+                Console.WriteLine("\n");
+            }
+            foreach (MobileAccount account in mobileList)
+            {
+                Console.WriteLine(account.getIznos() + "\t");
+                account.addToIznos(50);
+                Console.WriteLine(account.getIznos() + "\t");
+                account.subtractFromIznos(20);
+                Console.WriteLine(account.getIznos() + "\t");
+                Console.WriteLine("\n");
+            }
+        }
+        class BankAccount:IPayable
+        {
+            double iznos;
+            double maxMinus;
+            string ime;
+
+            public BankAccount(double iznos, double maxMinus, string ime)
+            {
+                this.iznos = iznos;
+                this.maxMinus = maxMinus;
+                this.ime = ime;
+            }
+            public double getIznos()
+            {
+                return this.iznos;
+            }
+            public void addToIznos(double iznos)
+            {
+                this.iznos += iznos;
+            }
+            public void subtractFromIznos(double iznos)
+            {
+                this.iznos -= iznos;
+            }
+        }
+        class MobileAccount:IPayable
+        {
+            string broj;
+            double stanje;
+            double cijenaSMS;
+            public MobileAccount(string broj, double stanje, double cijenaSMS)
+            {
+                this.broj = broj;
+                this.stanje = stanje;
+                this.cijenaSMS = cijenaSMS;
+                
+            }
+            public double getIznos()
+            {
+                return this.stanje;
+            }
+            public void addToIznos(double iznos)
+            {
+                this.stanje += iznos;
+            }
+            public void subtractFromIznos(double iznos)
+            {
+                this.stanje -= iznos;
+            }
+        }
+        interface IPayable
+        {
+            double getIznos();
+            void addToIznos(double iznos);
+            void subtractFromIznos(double iznos);
+        }
+    }
+}
---
+++ LV7/Zadatak 2.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LV7_Zad_2
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        bool check = false;
+        List<Pen> pens = new List<Pen>();
+        int i = -1;
+        public void update()
+        {
+            float width = hS.Value;
+            if (red.Checked)
+            {
+                pens.Add(new Pen(Color.Red, width));
+                check = true;
+            }
+            if (blue.Checked)
+            {
+                pens.Add(new Pen(Color.Blue, width));
+                check = true;
+            }
+            if (green.Checked)
+            {
+                pens.Add(new Pen(Color.Green, width));
+                check = true;
+            }
+            if(check)
+            i++;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            g = pbDrawing.CreateGraphics();
+        }
+    private void pbDrawing_Click(object sender, EventArgs e)
+        {
+            update();
+        }
+
+        private void pbDrawing_MouseUp_1(object sender, MouseEventArgs e)
+        {
+            
+            if (circle.Checked&&check)
+            {
+                update();
+                Circle c = new Circle();
+                c.draw(g, pens[i], e.X, e.Y);
+            }
+            if (square.Checked)
+            {
+                update();
+                Square s = new Square();
+                s.draw(g,pens[i],e.X,e.Y);
+            }
+            if (triangle.Checked)
+            {
+                update();
+                Triangle t = new Triangle();
+                t.draw(g, pens[i], e.X, e.Y);
+            }
+        }
+
+        private void hS_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+    class Circle
+    {
+        int r;
+        public Circle()
+        {
+            r = 100;
+        }
+        public void draw(Graphics g, Pen p, int x, int y)
+        {
+            g.DrawEllipse(p, x, y, r, r);
+        }
+    }  
+    class Square:IDrawable
+    {
+        public void draw(Graphics g, Pen p, int x, int y)
+        {
+            Point point1 = new Point(x, y);
+            Point point2 = new Point(x+100, y);
+            Point point3 = new Point(x+100, y+100);
+            Point point4 = new Point(x, y+100);
+            Point[] squarePoints =
+             {
+                 point1,
+                 point2,
+                 point3,
+                 point4
+             };
+            g.DrawPolygon(p,squarePoints);
+        }
+    }
+    class Triangle:IDrawable
+    {
+        public void draw(Graphics g, Pen p, int x, int y)
+        {
+            Point point1 = new Point(x, y);
+            Point point2 = new Point(x + 150, y);
+            Point point3 = new Point(x + 75, y - 120);
+            Point[] trianglePoints =
+             {
+                 point1,
+                 point2,
+                 point3
+             };
+            g.DrawPolygon(p, trianglePoints);
+        }
+    }
+    interface IDrawable
+    {
+        void draw(Graphics g, Pen p, int x, int y);
+    }
+   
+}
